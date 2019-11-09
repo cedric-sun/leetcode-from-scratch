8274e73abb39de951e5e197415dc8c0bf42005b7
Cedric Sun <sunce@ufl.edu>
2019-11-08 23:55:27 -0500

    init repo

---
+++ src/1.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdint.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int *ret =malloc(2*sizeof *ret);
+    *returnSize=2;
+
+    return ret;
+}
