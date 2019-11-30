4ab89bd5dcfa2149cec4cab0b5d8645372b8e3a4
Cedric Sun <sunce@ufl.edu>
2019-11-30 15:00:41 -0500

    done 26

---
+++ src/26.c
@@ -0,0 +1,8 @@
+int removeDuplicates( int* nums, int numsSize ) {
+    if ( numsSize == 0 ) return 0;
+    int sz = 1;
+    for ( int i = 1; i < numsSize; i++ )
+        if ( nums[i] != nums[i - 1] )
+            nums[sz++]=nums[i];
+    return sz;
+}
